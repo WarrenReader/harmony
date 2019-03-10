cd24c4dbf1572bc3261f7776e990c60581410be8
Warren Reader <warrendreader@gmail.com>
2019-03-09 22:21:38 -0700

    Initial Setup Of React App

---
+++ webapp/webpack.config.base.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Harmony',
+      filename: 'index.html',
+      template: './src/template.html'
+    })
+  ]
+};
